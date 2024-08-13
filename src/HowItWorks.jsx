c7a8f5dd266a3ee33e192038014c9cd0a981779f
clw12 <clw6444@gmail.com>
2024-08-13 13:56:17 +0100

    How it works tags

--- src/HowItWorks.jsx
+++ src/HowItWorks.jsx
@@ -1,45 +1,19 @@
+import Tile from './Tile'
+
 export default function HowItWorks() {
     return (
 
-        
+
         <section id="how-it-works" className="how-it-works-section">
             <h2 className="how-it-works-text">How It Works</h2>
             <div className="tiles">
-                <article className="tile">
-                    <img
-                        src="/images/how-it-works-1.png"
-                        alt="Phone call illustration"
-                        className="tile-image"
-                    />
-                    <h3>Give us a call...</h3>
-                    <p>
-                        Call us and book in a "Design Consultation" on a date
-                        and time that suits you.
-                    </p>
-                </article>
-                <article className="tile">
-                    <img
-                        src="/images/how-it-works-2.png"
-                        alt="Home visit illustration"
-                        className="tile-image"
-                    />
-                    <h3>We come to you...</h3>
-                    <p>
-                        We come to your home to do an assessment of the space
-                        and to your style preference.
-                    </p>
-                </article>
-                <article className="tile">
-                    <img
-                        src="/images/how-it-works-3.png"
-                        alt="Fireplace recommendation illustration"
-                        className="tile-image"
-                    />
-                    <h3>We recommend...</h3>
-                    <p>
-                        We send you a bespoke set of fireplace recommendations.
-                    </p>
-                </article>
+
+                <Tile image="" alt="Phone call illustration" title="Give us a call..." description="Call us and book in a Design Consultation on a date
+                        and time that suits you."/>
+
+                <Tile image="" alt="Home visit illustration" title="We come to you..." description="We come to your home to do an assessment of the space and to your style preference." />
+
+                <Tile image="" alt="Fireplace recommendation illustration" title="We recommend..." description="We send you a bespoke set of fireplace recommendations." />
             </div>
         </section>
     );
