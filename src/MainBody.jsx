10d9a652060c3190acc2eea3230d4090da7b21fa
Richard Lamb <lamby42@live.co.uk>
2024-08-13 12:30:18 +0100

    started breaking down html into separate components and folders - need to copy in the images to be local

--- src/MainBody.jsx
+++ src/MainBody.jsx
@@ -1,70 +1,18 @@
+import Hero from "./Hero";
+import Tile from "./Tile";
+
 export default function MainBody() {
     return (
         <main>
-            <section className="hero-section">
-                <img
-                    src="/images/hero-mobile.png"
-                    srcset="/images/hero-mobile.png 900w, /images/hero-desktop.png 901w"
-                    sizes="(max-width: 900px) 100vw,
-               901px"
-                    alt="Cozy living room with a fireplace"
-                    className="hero-image"
-                />
-
-                <div className="hero-description">
-                    <p className="hero-headline">
-                        Discover the <br />
-                        perfect fireplace ...
-                    </p>
-                    <p className="consultation-paragraph">
-                        Book consultation:{" "}
-                        <span className="number-span">0121 345 6789</span>
-                    </p>
-                </div>
-            </section>
+            <Hero />
+           
             <hr className="separator" />
 
-            <section id="how-it-works" className="how-it-works-section">
-                <h2 className="how-it-works-text">How It Works</h2>
-                <div className="tiles">
-                    <article className="tile">
-                        <img
-                            src="/images/how-it-works-1.png"
-                            alt="Phone call illustration"
-                            className="tile-image"
-                        />
-                        <h3>Give us a call...</h3>
-                        <p>
-                            Call us and book in a "Design Consultation" on a
-                            date and time that suits you.
-                        </p>
-                    </article>
-                    <article className="tile">
-                        <img
-                            src="/images/how-it-works-2.png"
-                            alt="Home visit illustration"
-                            className="tile-image"
-                        />
-                        <h3>We come to you...</h3>
-                        <p>
-                            We come to your home to do an assessment of the
-                            space and to your style preference.
-                        </p>
-                    </article>
-                    <article className="tile">
-                        <img
-                            src="/images/how-it-works-3.png"
-                            alt="Fireplace recommendation illustration"
-                            className="tile-image"
-                        />
-                        <h3>We recommend...</h3>
-                        <p>
-                            We send you a bespoke set of fireplace
-                            recommendations.
-                        </p>
-                    </article>
-                </div>
-            </section>
+            <Tile 
+            image="https://github.com/SchoolOfCode/frontend-eng-example/blob/main/images/how-it-works-1.png"
+            />
+           
+
         </main>
     );
 }
---
+++ src/Tile.jsx
@@ -0,0 +1,15 @@
+export default function Tile({ image, alt, title, description }) {
+    return (
+        <article className="tile">
+        <img
+            src={image}
+            alt={alt}
+            className="tile-image"
+        />
+        <h3>{title}</h3>
+        <p>
+            {description}
+        </p>
+    </article>
+    );
+}
